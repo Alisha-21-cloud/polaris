cb1535c8604eb623df99f11950e0545f12dbd1f3
Syed Ahmad Alisha <ahmadalishasyed@gmail.com>
2026-01-13 08:22:29 +0530

    10: code editor & state

--- src/features/projects/components/project-id-view.tsx
+++ src/features/projects/components/project-id-view.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils";
 
 import { Id } from "../../../../convex/_generated/dataModel";
 import { FileExplorer } from "./file-explorer";
+import { EditorView } from "@/features/editor/components/editor-view";
 
 
 const MIN_SIDEBAR_WIDTH = 200;
@@ -80,7 +81,7 @@ export const ProjectIdView = ({
               <FileExplorer projectId={projectId} />
             </Allotment.Pane>
             <Allotment.Pane>
-              <p>Editor view</p>
+              <EditorView projectId={projectId} />
             </Allotment.Pane>
           </Allotment>
         </div>
